f8e4ac78de785dc9ff7cebf086514712c8f4722b
YeEeck <yechenkai@foxmail.com>
2021-09-19 14:12:44 +0800

    init

---
+++ src/components/main.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>textText</div>
+</template>
+
+<script>
+export default {
+  name: "main",
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+      <v-app-bar
+        absolute
+        color="indigo darken-2"
+        dark
+        shrink-on-scroll
+        prominent
+        scroll-target="#scrolling-techniques"
+      >
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-app-bar-title style="width: 10rem">机器数转换器</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+      <div class="scroll-warp" id="scrolling-techniques" ref="warp1">
+        <v-container class="scroll-box-inner">
+          <y-main></y-main>
+        </v-container>
+      </div>
+    </v-app>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Main from "@/components/main";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    "y-main": Main,
+  },
+  mounted() {
+    this.$refs.warp1.style.height = window.innerHeight - 1 + "px";
+  },
+};
 </script>
+
+<style>
+.home {
+}
+.scroll-warp {
+  overflow-y: auto !important;
+  padding-top: 129px;
+}
+</style>
